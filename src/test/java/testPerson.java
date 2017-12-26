000db9967b4cab8ff2a3e7a29f018f8e3fde55ff
Kenny Ketelaar <kenny.ketelaar@gmail.com>
2017-12-26 18:49:07 +0100

    Added Travis and Maven

---
+++ src/test/java/testPerson.java
@@ -0,0 +1,17 @@
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+class testPerson {
+    @Test
+    void testTotal() {
+        Person p = new Person();
+        Card c1 = p.getHand().get(0);
+        Card c2 = p.getHand().get(1);
+        assertEquals(p.getTotal(), c1.getValue() + c2.getValue());
+    }
+
+
+}
